18802dfb241ab0c6e9a41d3d51c9083f2408ee3b
ysknsid25 <kengo071225@gmail.com>
2023-12-31 20:24:04 +0900

    15章
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -80,3 +80,22 @@ export async function deleteInvoice(id: string) {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
 }
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
+}
